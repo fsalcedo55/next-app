964b6b90dce303a2a89c73e97be3aef3dba9091d
fsalcedo55 <fsalcedo55@hotmail.com>
2022-08-26 16:17:45 -0400

    Add changes

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Page = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div>
+      <h1>Note {id}</h1>
+
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Page;
